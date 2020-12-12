15c3ab112dc8f486013301fc052215e9f6173a72
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2020-12-12 13:44:44 +0100

    refactor using complex numbers

--- 2020/src/day_12.jl
+++ 2020/src/day_12.jl
@@ -5,56 +5,44 @@ using Test, BenchmarkTools
 input = readlines("2020/data/day_12.txt")
 
 function part_1(input)
-    pos = [0, 0]
-    right = [1, 0]
-    up = [0, 1]
-    dir = 0
+    pos = 0 + 0im
+    right = 1 + 0im
+    up = 0 + 1im
+    dir = 1 + 0im
+    rot90 = 1im
     for line in input
         cmd, n = line[1], parse.(Int, line[2:end])
         if (cmd == 'N') pos += n * up
         elseif (cmd == 'S') pos -= n * up
         elseif (cmd == 'E') pos += n * right
         elseif (cmd == 'W') pos -= n * right
-        elseif (cmd == 'L') dir += n % 360
-        elseif (cmd == 'R') dir += (360 - n) % 360
-        elseif (cmd == 'F') 
-            if (dir == 0) pos += n * right
-            elseif (dir == 90) pos += n * up
-            elseif (dir == 180) pos -= n * right
-            elseif (dir == 270) pos -= n * up
-            else throw(ErrorException("dir=$dir"))
-            end
+        elseif (cmd == 'L') dir *= rot90^(n ÷ 90)
+        elseif (cmd == 'R') dir *= (-rot90)^(n ÷ 90)
+        elseif (cmd == 'F') pos += n * dir
         end
-        dir %= 360
     end
-    return sum(abs.(pos))
+    return abs(real(pos)) + abs(imag(pos))
 end
 @info part_1(input)
 
 function part_2(input)
-    pos = [0, 0]
-    right = [1, 0]
-    up = [0, 1]
-    waypoint = [10, 1]
-    rot90 = [0 -1; 1 0]
-    rotations = Dict(
-        90 => rot90,
-        180 => rot90^2,
-        270 => rot90^3,
-        0 => rot90^4
-    )
+    pos = 0 + 0im
+    right = 1 + 0im
+    up = 0 + 1im
+    waypoint = 10 + 1im
+    rot90 = 1im
     for line in input
         cmd, n = line[1], parse.(Int, line[2:end])
         if (cmd == 'N') waypoint += n * up
         elseif (cmd == 'S') waypoint -= n * up
         elseif (cmd == 'E') waypoint += n * right
         elseif (cmd == 'W') waypoint -= n * right
-        elseif (cmd == 'L') waypoint = rotations[n % 360] * waypoint
-        elseif (cmd == 'R') waypoint = rotations[(360 - n) % 360] * waypoint
+        elseif (cmd == 'L') waypoint *= rot90^(n ÷ 90)
+        elseif (cmd == 'R') waypoint *= (-rot90)^(n ÷ 90)
         elseif (cmd == 'F') pos += n * waypoint
         end
     end
-    return sum(abs.(pos))
+    return abs(real(pos)) + abs(imag(pos))
 end
 @info part_2(input)
 
