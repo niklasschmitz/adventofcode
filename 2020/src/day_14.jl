64a8162fc64885b44929b001cd9599f55c5eef0d
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2020-12-14 12:21:42 +0100

    add day 14 part 1

---
+++ 2020/src/day_14.jl
@@ -0,0 +1,39 @@
+# https://adventofcode.com/2020/day/14
+using AdventOfCode
+using Test, BenchmarkTools
+
+input = readlines("2020/data/day_14.txt")
+
+function part_1(input)
+    mask0 = 0
+    mask1 = 0
+    mem = Dict{Int,Int}()
+    for line in input
+        if line[1:4] == "mask"
+            mask0 = parse(Int, replace(line[end-35:end], "X"=>1), base=2)
+            mask1 = parse(Int, replace(line[end-35:end], "X"=>0), base=2)
+        else
+            addr, val = split(line, "] = ")
+            addr = parse(Int, addr[5:end])
+            val = parse(Int, val)
+            val |= mask1
+            val &= mask0
+            mem[addr] = val
+        end
+    end
+    return sum(values(mem))
+end
+@info part_1(input)
+
+function part_2(input)
+    nothing
+end
+@info part_2(input)
+
+@testset "day_14" begin
+    a = split("""mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+    mem[8] = 11
+    mem[7] = 101
+    mem[8] = 0""", "\n")
+    @test part_1(a) == 165
+end
