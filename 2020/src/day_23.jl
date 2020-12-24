e663b35117662825dd2776f68edfff67e65ddff7
Niklas Schmitz <niklas.f.schmitz@gmail.com>
2020-12-24 03:01:52 +0100

    day 23 optimize part 1&2

--- 2020/src/day_23.jl
+++ 2020/src/day_23.jl
@@ -5,91 +5,61 @@ using Test, BenchmarkTools
 
 input = parse.(Int, collect(readlines("2020/data/day_23.txt")[1]))
 
-mutable struct Cup
-    label::Int
-    next::Union{Cup, Nothing}
-end
-Cup(x::Int) = Cup(x, nothing)
-
-mutable struct Circle
-    curr::Cup
-end
-function Circle(cups::Vector{Cup})
-    cups = deepcopy(cups)
-    ncups = length(cups)
-    circle = Circle(first(cups))
-    for i in eachindex(cups)
-        cups[i].next = cups[mod1(i+1, ncups)]
-    end
-    return circle
-end
-function labels(c::Circle)
-    labels = [c.curr.label]
-    cup = c.curr.next
-    while cup != c.curr
-        push!(labels, cup.label)
-        cup = cup.next
-    end
-    return labels
-end
-function move!(c::Circle)
-    curr = c.curr
-    
-    # remove three next cups from circle
-    c1 = curr.next
-    c2 = c1.next
-    c3 = c2.next
-    curr.next = c3.next
-
-    # find extrema
-    lo = curr.label
-    hi = curr.label
-    cup = curr.next
-    while cup != curr
-        lo = min(lo, cup.label)
-        hi = max(hi, cup.label)
-        cup = cup.next
+function simulate(cup; moves=100)
+    # cup  : Idx   -> Label  "which cup is at I?"
+    # next : Label -> Label  "which is the succ of L?"
+    next = similar(cup)
+    for i in 1:length(cup)
+        next[cup[i]] = cup[mod1(i+1,length(cup))]
     end
 
-    # select destination
-    dest = curr
-    label = curr.label - 1
-    while (dest.label != label)
-        dest = dest.next
-        any(label .== (c1.label,c2.label,c3.label)) && (label -= 1)
-        (label < lo) && (label = hi)
+    current = cup[1]
+    for move in 1:moves
+        # remove three next cups from circle
+        c1 = next[current]
+        c2 = next[c1]
+        c3 = next[c2]
+        next[current] = next[c3]
+
+        # select destination
+        dest = mod1(current - 1, length(cup))
+        while dest in (c1,c2,c3)
+            dest = mod1(dest - 1, length(cup))
+        end
+
+        # insert after destination
+        next[c3] = next[dest]
+        next[dest] = c1
+
+        # advance current cup
+        current = next[current]
     end
-
-    # insert after destination
-    c3.next = dest.next
-    dest.next = c1
-
-    # advance current cup
-    c.curr = c.curr.next
+    return next
 end
 
 function part_1(input; moves=100)
-    cups = Cup.(input)
-    circle = Circle(cups)
-    for i in 1:moves
-        move!(circle)
+    next = simulate(input; moves=moves)
+    i = next[1]
+    res = zeros(Int, length(input)-1)
+    for j in 1:length(input)-1
+        res[j] = i
+        i = next[i]
     end
-    l = labels(circle)
-    return join(circshift(l, 1-findfirst(==(1), l))[2:end])
+    return join(res) # 20 allocs
 end
 
-function part_2(input)
-    nothing
+function part_2(input; moves=10_000_000)
+    next = simulate(input; moves=moves)
+    i = next[1]
+    return i * next[i]
 end
-@info part_2(input)
 
 @testset "day_23" begin
     a = parse.(Int, collect("389125467"))
-    @test part_1(input; moves=10) == "92658374"
-    @test part_1(input; moves=100) == "67384529"
+    @test part_1(a; moves=10) == "92658374"
+    @test part_1(a; moves=100) == "67384529"
+    @test part_2([a; collect(10:1_000_000)]) == 149245887792
 end
 
-# @time part_1(input; moves=1000000);
-# @time part_1(vcat(input, 10:1000); moves=1000000);
-
-part_1(input)
+@btime part_1(input; moves=100)
+@btime part_2([input; collect(10:1_000_000)])
